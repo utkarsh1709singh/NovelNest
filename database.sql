d174dc2ce2108bfbd7e4deaef0a2da45f9267757
z124075 <167387012+z124075@users.noreply.github.com>
2024-07-14 16:06:45 +0900

    added books dataset

--- database.sql
+++ database.sql
@@ -1,13 +1,30 @@
 
-CREATE DATABASE userData;
+CREATE DATABASE novelNestDb;
 
 DROP TABLE IF EXISTS users; 
 
 CREATE TABLE users (
-    username varchar(255),
-    pass varchar(255),
-    wishlist int,
-    completed int,
-    dropped int,
-    current int
+    username VARCHAR(255),
+    pass VARCHAR(255),
+    wishlist NUMERIC,
+    completed NUMERIC,
+    dropped NUMERIC,
+    current NUMERIC 
 );
+
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    bookID NUMERIC,
+    title VARCHAR(255),
+    authors VARCHAR(255),
+    average_rating float,
+    isbn NUMERIC,
+    isbn13 NUMERIC,
+    language_code CHAR(3),
+    num_pages NUMERIC,
+    ratings_count NUMERIC,
+    text_reviews_count NUMERIC
+);
+
+\copy books FROM 'books.csv' DELIMITER ',' CSV HEADER;
